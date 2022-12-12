fc3ac978bcb0a45d65870fa7100203ac9e7aea57
ryuda <ryuda0101@naver.com>
2022-12-12 18:09:54 +0900

    갤러리 기능 추가

--- src/component/Container.js
+++ src/component/Container.js
@@ -1,26 +1,23 @@
 import React from 'react'
-import Cont1 from './Cont1'
-import Cont2 from './Cont2'
-import Cont3 from './Cont3'
-import Cont4 from './Cont4'
-import Cont5 from './Cont5'
+import MainCont from './MainCont'
 
 import { useEffect } from 'react'
 import "aos/dist/aos.css"
 import AOS from 'aos';
+import { Route, Routes } from 'react-router-dom';
+import Gallery from './Gallery';
 
 const Container = () => {
-  // 이렇게 aos를 넣으면 cont1 ~ cont5 어디서든 aos를 쓸 수 있다.
   useEffect(() => {
     AOS.init();
   });
   return (
     <div id='container'>
-      <Cont1></Cont1>
-      <Cont2></Cont2>
-      <Cont3></Cont3>
-      <Cont4></Cont4>
-      <Cont5></Cont5>
+      <Routes>
+        {/* path='경로' element={보여줄 컴포넌트} */}
+        <Route path='/' element={<MainCont></MainCont>}></Route>
+        <Route path='/Gallery' element={<Gallery></Gallery>}></Route>
+      </Routes>
     </div>
   )
 }
